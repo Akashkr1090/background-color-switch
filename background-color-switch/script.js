0df97a736b8b16906b50cb82d31edf8b15496de9
Akashkr1090 <95604926+Akashkr1090@users.noreply.github.com>
2022-06-14 17:03:42 +0530

    Add files via upload

---
+++ background-color-switch/script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+console.log(buttons);
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'lightgreen'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'aqua'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'black'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
